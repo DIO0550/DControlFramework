9c59920ac1ad0ff499f1273e7d2fbd8a9813a679
DIO <>
2019-11-03 16:50:37 +0900

    ファイルをsrcに移動。サンプル用のプロジェクトファイル追加

--- src/DControl/DControl/OnOffTypeButton.swift
+++ src/DControl/DControl/OnOffTypeButton.swift
@@ -1,6 +1,6 @@
 //
 //  OnOffTypeButton.swift
-//  DControl
+//  DSleepingControlFramework
 //  MacOSのTypeOnOff風のボタン
 //  Created by DIO on 2019/10/27.
 //  Copyright © 2019 DIO. All rights reserved.
@@ -8,9 +8,8 @@
 
 import UIKit
 
-@available(iOS 13.0, *)
 @IBDesignable
-class OnOffTypeButton: UIButton {
+public class OnOffTypeButton: UIButton {
     
     // ONの場合の背景色
     @IBInspectable var onStateBgColor:UIColor = UIColor.systemBlue
@@ -25,12 +24,12 @@ class OnOffTypeButton: UIButton {
     // ONの場合の枠線の色
     @IBInspectable var onStateBorderColor:UIColor = UIColor.cyan
     // OFFの場合の枠線の色
-    @IBInspectable var offStateBorderColor:UIColor = UIColor.link
+    @IBInspectable var offStateBorderColor:UIColor = UIColor.systemBlue
     
     // 枠線の太さ
     @IBInspectable var borderWidth:CGFloat = 1.0
     // ボタンのステータス
-    private(set) var buttonState:Bool = false {
+    @IBInspectable private(set) var buttonState:Bool = false {
         willSet {
             if newValue {
                 self.backgroundColor = self.onStateBgColor
@@ -48,13 +47,17 @@ class OnOffTypeButton: UIButton {
     @IBInspectable var cornerRadius:CGFloat = 0
     
     
-    override func awakeFromNib() {
+    override public func awakeFromNib() {
         super.awakeFromNib()
         
         self.clipsToBounds = true
         self.layer.borderWidth = self.borderWidth
         self.layer.cornerRadius = self.cornerRadius
         self.layer.borderColor = self.offStateBorderColor.cgColor
+        // buttonStateのwillSet呼び出すために、同じ値を入れる
+        let state = self.buttonState
+        self.buttonState = state;
+        
         self.addTarget(self, action: #selector(self.changeButtonState), for: .touchUpInside)
     }
     
